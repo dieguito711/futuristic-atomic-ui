c3e997fe03157618b03cdb38aaafbf6fa83af124
AgustinaGV <agustinagarciavega@gmail.com>
2020-10-01 10:51:26 -0300

    Update styled inputs

--- src/components/InputSelect/styled.js
+++ src/components/InputSelect/styled.js
@@ -3,6 +3,7 @@ import { motion } from "framer-motion";
 import { leerColor, colores } from "../../constants";
 
 const InputSelectContainer = styled.select `
+padding: 0.5em 2em 0.5em 2em;
 font-size:1em;
 color: ${leerColor(colores.violeta1)};
 font-family: "Montserrat", cursive;
@@ -12,20 +13,26 @@ border-image-slice: 1;
 border-width: 0px 0px 3px 0px;
 border-color: ${leerColor(colores.violeta1)};
 
+:hover {
+
+    background-color: #FCFAFA;
+    border-color: linear-gradient( to right, 
+      ${leerColor(colores.violeta1)},
+      ${leerColor(colores.violeta2)});
+}
+
+:focus {
+
+    outline: none;
+}
+
 option {
 
     color: ${leerColor(colores.violeta1)};
+    background-color: #FCFAFA;
 }
 
 `;
 
 
-const DivInputSelectContainer = styled.div `
-border: none;
-margin: 0 !important;
-min-width: 30em !important;
-
-
-`;
-
-export { InputSelectContainer, DivInputSelectContainer } ;
+export { InputSelectContainer } ;
