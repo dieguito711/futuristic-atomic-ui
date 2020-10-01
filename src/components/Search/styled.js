110241098a3377bd5c2d6497914421758b814230
dieguito711 <diegosalischiker711@gmail.com>
2020-10-01 11:02:53 -0300

    Update Search

--- src/components/Search/styled.js
+++ src/components/Search/styled.js
@@ -3,20 +3,24 @@ import { motion } from "framer-motion";
 import { leerColor, colores } from "../../constants";
 import { BsSearch } from "react-icons/bs";
 
+const SearchContainer = styled.input`
+  font-size: 1em;
+  color: black;
+  font-family: "Montserrat", cursive;
+  text-align: left;
+  border: none;
+`;
 
-const SearchContainer = styled.input `
-font-size:1em;
-color: black;
-font-family: "Montserrat", cursive;
-text-align: right !important;
-border: solid;
-border-image-slice: 1;
-border-width: 0px 0px 3px 0px;
-border-color: black;
-:focus {
-    color:${leerColor(colores.violeta1)};
+const DivSearchContainer = styled.div`
+  border: solid;
+  border-image-slice: 1;
+  border-width: 0px 0px 3px 0px;
+  border-color: black;
+  :focus {
+    color: ${leerColor(colores.violeta1)};
     outline: none;
-    border-image-source: linear-gradient( to right, 
+    border-image-source: linear-gradient(
+      to right,
       ${leerColor(colores.verde2)},
       ${leerColor(colores.verde1)},
       ${leerColor(colores.amarillo1)},
@@ -25,12 +29,14 @@ border-color: black;
       ${leerColor(colores.rojo1)},
       ${leerColor(colores.rojo2)},
       ${leerColor(colores.violeta1)},
-      ${leerColor(colores.violeta2)});
-}
-:hover {
-    color:${leerColor(colores.violeta1)};
+      ${leerColor(colores.violeta2)}
+    );
+  }
+  :hover {
+    color: ${leerColor(colores.violeta1)};
     outline: none;
-    border-image-source: linear-gradient( to right, 
+    border-image-source: linear-gradient(
+      to right,
       ${leerColor(colores.verde2)},
       ${leerColor(colores.verde1)},
       ${leerColor(colores.amarillo1)},
@@ -39,24 +45,20 @@ border-color: black;
       ${leerColor(colores.rojo1)},
       ${leerColor(colores.rojo2)},
       ${leerColor(colores.violeta1)},
-      ${leerColor(colores.violeta2)});
-}
+      ${leerColor(colores.violeta2)}
+    );
+  }
+  margin: 0 !important;
+  width: fit-content;
+  :focus {
+    color: ${leerColor(colores.violeta1)};
+  }
+  :active {
+    color: ${leerColor(colores.violeta1)};
+  }
+  :hover {
+    color: ${leerColor(colores.violeta1)};
+  }
 `;
 
-const DivSearchContainer = styled.div `
-border: none;
-margin: 0 !important;
-width: 15em !important;
-:focus {
-    color:${leerColor(colores.violeta1)};
-};
-:active {
-    color:${leerColor(colores.violeta1)};
-};
-:hover {
-    color:${leerColor(colores.violeta1)};
-}
-
-`;
-
-export { SearchContainer, DivSearchContainer } ;
+export { SearchContainer, DivSearchContainer };
