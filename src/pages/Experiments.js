c8869c2c5ffc67baea5131ddc021b2841e4c8be4
AgustinaGV <agustinagarciavega@gmail.com>
2020-10-08 09:55:20 -0300

    Update

--- src/pages/Experiments.js
+++ src/pages/Experiments.js
@@ -1,1 +1,32 @@
+import React, { useEffect, useState } from "react";
+import Layout from "../containers/Layout";
+import Section from "../components/Section";
+import Button from "../components/Button";
+import Titles from "../components/Titles";
+import Paragraph from "../components/Paragraph";
+import Quote from "../components/Quote";
+import Search from "../components/Search";
+import InputText from "../components/InputText";
+import InputTextarea from "../components/InputTextarea";
+import InputSelect from "../components/InputSelect";
+
+import Image from "../components/Image";
+import Card from "../components/Card";
+
+
+const Experiments = () => {
+  return (
+    <Layout>
+      <Section>
+        <Titles type="h1">
+            Experiments
+        </Titles>
+      </Section>
+      <Section>
+        <Paragraph>Proximamente amiwi</Paragraph>
+      </Section>
+    </Layout>
+  );
+};
+export default Experiments;
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -19,11 +19,13 @@ const Home = () => {
     <Layout>
       <Section>
         <Titles type="h1">
-          Home
+          Futuristic UI
         </Titles>
       </Section>
       <Section>
-
+        <Paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et ante eget mauris mattis hendrerit. Sed sed hendrerit sem, non aliquam mi. Aenean sit amet nisi ante. Donec vitae lacus ut tortor efficitur consectetur eget non dui. Nunc aliquet ac erat convallis scelerisque. Nullam facilisis leo sed pretium consectetur. Nulla in gravida justo, vel ornare leo. Ut eu imperdiet metus. Nunc posuere elit eget ex rutrum, ac pharetra nulla sodales. Praesent mauris mi, sagittis accumsan sem eget, tincidunt aliquet ante. Mauris vehicula dui a felis facilisis, ac egestas purus malesuada. Maecenas elit dui, dignissim id vestibulum vitae, sodales at quam.
+        </Paragraph>
       </Section>
     </Layout>
   );
